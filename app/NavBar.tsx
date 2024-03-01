4f7a9a0fbd19c273170fc334f9fca60d6a74664b
varidhsrivastav <varidhsrivastava19145@gmail.com>
2024-03-01 11:09:54 +0530

    prisma update

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,17 +1,34 @@
+'use client';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react';
 import {AiFillBug } from 'react-icons/ai'
+import classNames from 'classnames';
  
 const NavBar = () => {
+  const currrentpath =usePathname();
+  const links=[
+    {lable:'Dashboard', href:'/'},
+    {lable:'Issues', href:'/issues'}
+  ]
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-      <Link href="/">
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center container '>
+      <Link href="/" className=''>
         <AiFillBug/>
       </Link>
       <ul className='flex  space-x-6'>
-        <li><Link href="/">Dashboard</Link></li>
-        <li><Link href="/issues">Issues</Link></li>
-        <li><Link href="/">Dashboard</Link></li>
+        {
+          links.map(links=>
+          <Link key={links.href}
+          href={links.href}
+          className={classNames({
+            ' dark:text-gray-500':currrentpath!==links.href,
+            ' text-gray-900':currrentpath===links.href,
+            'hover:text-zinc-800 transition-colors':true
+          })}>
+            {links.lable}
+          </Link>)
+        }
       </ul>
     </nav>
   )
---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IssuesPage = () => {
+  return (
+    <div>
+      <h1>issue page</h1>
+    </div>
+  )
+}
+
+export default IssuesPage
